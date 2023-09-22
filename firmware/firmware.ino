e082f98c3c126e996cc66bf34fcf0bcf407ca312
Jonáš Rosecký <rosecky.jonas@gmail.com>
2023-09-22 21:37:06 +0200

    Added party mode

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -183,15 +183,57 @@ void loop() {
   }
   if (state == 4) {
     // Party sequence
+    Serial.print(symbols[0]); Serial.print(", ");
+    Serial.print(symbols[1]); Serial.print(", ");
+    Serial.print(symbols[2]); Serial.print(", ");
+    Serial.print(symbols[3]); Serial.print(", ");
+    Serial.print(symbols[4]); Serial.print(", ");
+    Serial.print(symbols[5]); Serial.print(", ");
+    Serial.print(symbols[6]); Serial.println("");
     if (symbols[0] == 13 && symbols[1] == 14 && symbols[2] == 15 && symbols[3] == 16 && symbols[4] == 17 && symbols[5] == 18 && symbols[6] == 19) {
       state = 5;
       progress = 0;
     }
   }
   if (state == 5) {
-    for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+    int modes = 10;
+    if (ptr % modes == 0) {
+      colorWipe(strip.Color(255,   0,   0), 10);
+      colorWipe(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 1) {
+      colorWipe(strip.Color(  0, 255,   0), 10);
+      colorWipe(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 2) {
+      colorWipe(strip.Color(  0,   0, 255), 10);
+      colorWipe(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 3) {
+      colorWipe(strip.Color(255, 255, 255), 10);
+      colorWipe(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 4) {
+      theaterChase(strip.Color(255,   0,   0), 10);
+      theaterChase(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 5) {
+      theaterChase(strip.Color(  0, 255,   0), 10);
+      theaterChase(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 6) {
+      theaterChase(strip.Color(  0,   0, 255), 10);
+      theaterChase(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 7) {
+      theaterChase(strip.Color(255, 255, 255), 10);
+      theaterChase(strip.Color(  0,   0,   0), 10);
+    }
+    if (ptr % modes == 8) {
       strip.rainbow(256 * progress);
-      strip.show();
+    }
+    if (ptr % modes == 9) {
+      theaterChaseRainbow(50);
     }
   }
 
@@ -223,4 +265,67 @@ void doEncoder() {
       ptr += SYMBOLS;
     }
   }  
+}
+
+
+//// From strandtest.ino - Neopixel
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  int ptrLoc = ptr;
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+    if (ptr != ptrLoc) return;
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  int ptrLoc = ptr;
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+      if (ptr != ptrLoc) return;
+    }
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int ptrLoc = ptr;
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      if (ptr != ptrLoc) return;
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
 }
